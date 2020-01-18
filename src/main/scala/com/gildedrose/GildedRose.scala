36aade839941050139c9e05b592e0bc20dcb9a28
matkenis <rutenisnaug@gmail.com>
2020-01-19 01:42:12 +0200

    Added:  -items are now represented in a map with a semantic meaning of a specific category  -each item category update function in a PartialFunction form

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,57 +1,47 @@
 package com.gildedrose
 
-class GildedRose(val items: Array[Item]) {
+import GildedRose._
 
-  def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
-        }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
+object GildedRose {
+  sealed trait Category
+  case object Conjured extends Category
+  case object Legendary extends Category
+  case object Common extends Category
+  case object BackStagePass extends Category
 
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
+  val ITEMS: Map[Category, Vector[String]] = {
+    Map(
+      Legendary -> Vector("Sulfuras, Hand of Ragnaros"),
+      Conjured -> Vector("Conjured Mana Cake"),
+      Common -> Vector("Aged Brie", "Elixir of the Mongoose", "+5 Dexterity Vest"),
+      BackStagePass -> Vector("Backstage passes to a TAFKAL80ETC concert")
+    )
+  }
+}
 
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
+class GildedRose(val items: Vector[Item]) {
 
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
-      }
+  def updateQuality(): Unit = {
+    items.map {
+      updateCommon orElse
+      updateLegendary orElse
+      updateConjured
     }
+  }
+
+  val updateCommon: PartialFunction[Item, Item] = {
+    case Item(name, quality, sellIn) if ITEMS(Common).contains(name) => Item(name, quality - 1, sellIn - 1)
+  }
+
+  val updateLegendary: PartialFunction[Item, Item] = {
+    case Item(name, quality, sellIn) if ITEMS(Legendary).contains(name) => Item(name, quality, sellIn)
+  }
+
+  val updateConjured: PartialFunction[Item, Item] = {
+    case Item(name, quality, sellIn) if ITEMS(Conjured).contains(name) => Item(name, quality, sellIn)
+  }
+
+  val updateBackStagePass: PartialFunction[Item, Item] = {
+    case Item(name, quality, sellIn) if ITEMS(BackStagePass).contains(name) => Item(name, quality, sellIn)
   }
 }
--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 
 class GildedRoseTest  extends FlatSpec with Matchers {
       it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
+        var items = Vector[Item](new Item("foo", 0, 0))
         val app = new GildedRose(items)
         app.updateQuality()
         (app.items(0).name) should equal ("fixme")

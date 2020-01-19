1294d043de6bd655f19af7dc0c3135d9d4d94a7a
matkenis <rutenisnaug@gmail.com>
2020-01-19 03:34:38 +0200

    Add  -Each specific category quality and sellIn change (over night) logic  -updateQuality method to 'Conjured', 'Common' and 'BackStagePass' objects

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,24 +1,28 @@
 package com.gildedrose
 
 import ItemsClassifier._
+import GildedRose._
 
 object GildedRose {
-  private def decrease(value: Int, amount: Int) = {
-    if (value > amount)
-      value - amount
+  //Quality or sellIn will never be negative
+  def decrease(value: Int, by: Int) = {
+    if (value > by)
+      value - by
     else
       0
   }
 
-  private def increase(value: Int, amount: Int, threshold: Int) = {
-    val newQuality = value + amount
-    if (value + amount <= threshold)
+  def increase(value: Int, by: Int, threshold: Int) = {
+    val newQuality = value + by
+    if (value + by <= threshold)
       newQuality
     else
       threshold
   }
 }
 
+/*We can use Partial functions .isDefined to represent which Item is
+  being iterated since we can not modify Item class to extend anything*/
 class GildedRose(val items: Vector[Item]) {
   def updateQuality(): Unit = {
     items.map {
@@ -30,18 +34,22 @@ class GildedRose(val items: Vector[Item]) {
   }
 
   private val updateCommon: PartialFunction[Item, Item] = {
-    case Item(name, quality, sellIn) if ITEMS(Common).contains(name) => Item(name, quality - 1, sellIn - 1)
-  }
-
-  private val updateLegendary: PartialFunction[Item, Item] = {
-    case Item(name, quality, sellIn) if ITEMS(Legendary).contains(name) => Item(name, quality, sellIn)
+    case item @ Item(name, sellIn, quality) if ITEMS(Common).contains(name) =>
+      item.copy(name, decrease(sellIn, 1), Common.updateQuality(quality))
   }
 
   private val updateConjured: PartialFunction[Item, Item] = {
-    case Item(name, quality, sellIn) if ITEMS(Conjured).contains(name) => Item(name, quality, sellIn)
+    case item @Item(name, sellIn, quality) if ITEMS(Conjured).contains(name) =>
+      val by = if (sellIn <= 0) 4 else 2
+      item.copy(name, decrease(sellIn, 1), Conjured.updateQuality(quality, by))
   }
 
   private val updateBackStagePass: PartialFunction[Item, Item] = {
-    case Item(name, quality, sellIn) if ITEMS(BackStagePass).contains(name) => Item(name, quality, sellIn)
+    case Item(name, sellIn, quality) if ITEMS(BackStagePass).contains(name) =>
+      Item(name, decrease(sellIn, 1), BackStagePass.updateQuality(sellIn, quality))
+  }
+
+  private val updateLegendary: PartialFunction[Item, Item] = {
+    case item: Item if ITEMS(Legendary).contains(item.name) => item
   }
 }
--- src/main/scala/com/gildedrose/ItemsClassifier.scala
+++ src/main/scala/com/gildedrose/ItemsClassifier.scala
@@ -1,11 +1,31 @@
 package com.gildedrose
 
+import GildedRose._
+
 object ItemsClassifier {
-  sealed trait Category
-  case object Conjured extends Category
+  trait Category
   case object Legendary extends Category
-  case object Common extends Category
-  case object BackStagePass extends Category
+  case object Conjured extends Category {
+    def updateQuality(sellIn: Int, quality: Int): Int = {
+      val by = if (sellIn <= 0) 4 else 2
+      decrease(quality, by)
+    }
+  }
+  case object Common extends Category {
+    def updateQuality(quality: Int): Int = decrease(quality, 1)
+  }
+  case object BackStagePass extends Category {
+    def updateQuality(sellIn: Int, quality: Int): Int = {
+      val by =
+        if (sellIn > 10) Some(1) else
+        if (sellIn <= 10 && sellIn >= 5) Some(2) else
+        if (sellIn <= 5 && sellIn > 0) Some(3) else None
+
+      by.fold(0)(by => increase(quality, by, 50))
+    }
+  }
+  /*
+      item.copy(name, decrease(sellIn, by), decrease(quality, 1))*/
 
   /*There could be over +1000 items. Putting them into case object's doesn't sound reasonable.
     A viable solution could be to store them into some sort of a serialized format (maybe JSON)

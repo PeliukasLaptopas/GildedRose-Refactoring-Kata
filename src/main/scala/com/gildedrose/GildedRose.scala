5141e55e35ac12a821bcf08b7164cd3f7bd07029
matkenis <rutenisnaug@gmail.com>
2020-01-20 12:47:12 +0200

    Cleanup:  -Simple cleanup

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -10,7 +10,7 @@ class GildedRose(val initialItems: Vector[Item]) {
     @tailrec
     def go(daysLeft: Int, acc: Vector[Item]): Either[GildedError, Vector[Item]] = {
       if (daysLeft > 0) {
-        ItemUpdateManager.updateItems(acc) match { //This is not written in "updateQuality(acc).fold(<..>)" and instead in a 'match' because this needs to be a tailrec
+        ItemUpdateManager.updateItems(acc) match { //This is not written in "ItemUpdateManager.updateItems(acc).fold(<..>)" and instead in a 'match' because this needs to be a tailrec
           case Right(updatedItems) => go(daysLeft - 1, updatedItems)
           case err@ Left(_) => err
         }
